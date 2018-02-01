62534af3f8029fba3f4bff1b2544629a7107c274
John Jessop <jessopjohn1@gmail.com>
2018-02-01 20:01:42 +0000

    Added some more pythagoras
    Added pythagoras for sides that arent the hypotenuse

--- main.py
+++ main.py
@@ -12,48 +12,26 @@ print("Pythagoras' theorum = 3")
 mode=int(input("Which mode would you like to use: "))
 
 if mode == "1":
-    #ask user for their fist number
-    numb = int(input("What is your first number? "))
-    mylist2.append(numb)
-    #Asks which operation they would like to perform
-    print("1 = Add")
-    print("2 = Subtract")
-    print("3 = Divide")
-    print("4 = Times")
-    print("5 = Square")
-    print("6 = Cube")
-    print("7 = Square root")
-    print("8 = Cube root")
-
-    #Ask user which operator they would like to use
-    operator = input("What operator would you like to use? ")
-    #If square/cube or square/cube root work out answer and end
-    if operator == "5":
-        print(numb," squared  is", int(numb)**2)
-    elif operator == "6":
-        print (numb, " cubed is",  int(numb)**3)
-    elif operator == "7":
-        print("The square root of ", numb, " is ", int(numb)**(1/2))
-    elif operator == "8":
-        print("The cube root of ", numb, " is ", int(numb)**(1/3))
-    #Asks for second number then works out the answer
-    numb2 = input("What is your second number? ")
     elif operator == "1":
+
         while (done == "n"):
             numb2 = int(input("What is your next number? "))
             done = input("Are you done yet? [y/n]")
             mylist.append(num2)
         print("Sum of ", mylist2, " = ", sum(mylist2))
     elif operator == "2":
+
         print(numb,"-",numb2,"=", int(numb)-int(numb2))
     elif operator == "3":
+
         print(numb,"/",numb2,"=", int(numb)/int(numb2))
     elif operator == "4":
-        print(numb,"*",numb2,"=", int(numb)*int(numb2))
 
+        print(numb,"*",numb2,"=", int(numb)*int(numb2))
 
     else:
         print("You did something wrong try again and make sure you read my rules.")
+
 elif mode == "2":
     print("At the moment I can only work out the mean I will try and add more at a later date")
     num = int(input("Please input your first number: "))
@@ -66,12 +44,25 @@ elif mode == "2":
 
     print("The mean of your numbers is ", sum(mylist)/len(mylist))
 elif mode == "3":
-    side=input("How long is one of the sides (that you know) ")
-    side2 = input("How long is the other side? ")
-    print("The sum to work out the hypotenuse is as follows A squared + B squared = C squared")
-    hypotenuse=int(side)*int(side)+int(side2)*int(side2)
-    print("The length of the hypotenuse is ", hypotenuse**(1/2))
+    type = input.lower("Do you want to find out the length of the hypotenuse or one of the other side? [other/hyp]")
+
+    if type = "hype":
+        side = input("How long is one of the sides (that you know) ")
+        side2 = input("How long is the other side? ")
+        print("The sum to work out the hypotenuse is as follows A squared + B squared = C squared")
+        unknown=int(side)**2+int(side2)**2
+        print("The length of the hypotenuse is ", unknown**(1/2))
+
+    elif type = "other":
+        side = input("How long is one of the sides (the longer of the two) ")
+        side2 = input("How long is the other side? ")
+        print("The sum to work out the hypotenuse is as follows A squared + B squared = C squared")
+        unknown=int(side)**2-int(side2)**2
+        print("The length of the hypotenuse is ", unknown**(1/2))
+
+    else:
+        print("Incorrect command please start again")
 
 else:
-    print("You did something wrong please start again")
+    print("Incorrect command please start again")
 os.system("pause")
