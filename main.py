a7829c422190e63e1e9cd930962a6c238d424ea7
John Jessop <jessopjohn1@gmail.com>
2018-02-01 20:12:30 +0000

    It was broken now its not
    I Fixed it because it was broken

--- main.py
+++ main.py
@@ -1,24 +1,24 @@
 import os
-mylist=[]
-mylist2=[]
+additionList=[]
+additionList2=[]
 print("This is John's awsome (basic) calculator here are a few ground rules:")
-print("1. Only use two numbers")
-print("2. No fractions")
+print("Only use two numbers")
+print("No fractions")
 print("I can do lots of different types of things including working out the length of a hypotenuse of a right angled triangle, I can also calculate different types of averages, or I can do normal calculator things like addition, subtraction, division, multiplication, square and cube rooting and a few other things")
 print("Normal = 1")
 print("Averages = 2")
 print("Pythagoras' theorum = 3")
 #asks user what they want to do
-mode=int(input("Which mode would you like to use: "))
+mode=input("Which mode would you like to use: ")
 
 if mode == "1":
-    elif operator == "1":
+    if operator == "1":
 
         while (done == "n"):
             numb2 = int(input("What is your next number? "))
             done = input("Are you done yet? [y/n]")
-            mylist.append(num2)
-        print("Sum of ", mylist2, " = ", sum(mylist2))
+            additionList.append(num2)
+        print("Sum of ", additionList2, " = ", sum(additionList2))
     elif operator == "2":
 
         print(numb,"-",numb2,"=", int(numb)-int(numb2))
@@ -35,28 +35,28 @@ if mode == "1":
 elif mode == "2":
     print("At the moment I can only work out the mean I will try and add more at a later date")
     num = int(input("Please input your first number: "))
-    mylist.append(num)
+    additionList.append(num)
     done = "n"
     while (done == "n"):
         num2 = int(input("What is your next number? "))
         done = input("Are you done yet? [y/n]")
-        mylist.append(num2)
+        additionList.append(num2)
 
-    print("The mean of your numbers is ", sum(mylist)/len(mylist))
+    print("The mean of your numbers is ", sum(additionList)/len(additionList))
 elif mode == "3":
-    type = input.lower("Do you want to find out the length of the hypotenuse or one of the other side? [other/hyp]")
+    type = input("Do you want to find out the length of the hypotenuse or one of the other side? [other/hyp]")
 
-    if type = "hype":
+    if type == "hyp":
         side = input("How long is one of the sides (that you know) ")
         side2 = input("How long is the other side? ")
         print("The sum to work out the hypotenuse is as follows A squared + B squared = C squared")
         unknown=int(side)**2+int(side2)**2
         print("The length of the hypotenuse is ", unknown**(1/2))
 
-    elif type = "other":
+    elif type == "other":
         side = input("How long is one of the sides (the longer of the two) ")
         side2 = input("How long is the other side? ")
-        print("The sum to work out the hypotenuse is as follows A squared + B squared = C squared")
+        print("The sum to work out the hypotenuse is as follows C squared + A squared = B squared")
         unknown=int(side)**2-int(side2)**2
         print("The length of the hypotenuse is ", unknown**(1/2))
 
